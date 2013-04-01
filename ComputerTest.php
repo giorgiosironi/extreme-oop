d9962075162426d4fa4b5a7a7a89d68f94436bb2
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2013-04-01 15:31:44 +0200

    Failing test for story 2: single print statement

--- ComputerTest.php
+++ ComputerTest.php
@@ -1,7 +1,4 @@
 <?php
-require_once 'Computer.php';
-require_once 'Program.php';
-require_once 'Output.php';
 
 class ComputerTest extends \PHPUnit_Framework_TestCase
 {
@@ -10,5 +7,12 @@ class ComputerTest extends \PHPUnit_Framework_TestCase
         $computer = new Computer();
         $program = new Program('');
         $this->assertEquals(new Output(''), $computer->execute($program));
+    } 
+
+    public function testABarePrintStatementProducesASingleNewline()
+    {
+        $computer = new Computer();
+        $program = new Program('');
+        $this->assertEquals(new Output("\n"), $computer->execute('PRINT'));
     } 
 }
