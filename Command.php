2067e1ee40b293e7fa5846f2530e1eece131a04d
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2013-04-01 16:24:48 +0200

    Document interface Command

--- Command.php
+++ Command.php
@@ -2,6 +2,13 @@
 
 interface Command
 {
+    /**
+     * @return boolean
+     */
     public function match(Statement $statement);
+
+    /**
+     * @return Output
+     */
     public function execute(Statement $statement);
 }
